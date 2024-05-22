e8d734b1d03c6ade2ddd8c2d8f7bde1f225b8ff2
SantheepG <gshantheep@gmail.com>
2024-05-22 14:25:22 +0530

    migrated vars to .env

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, jsonify, request
 import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 app = Flask(__name__)
 
-#fb page id : 291374310732336
-#insta page id : 17841466466174309
 
-page_access_tkn = "EAAGWZA4XTypwBO7UfFPeDfwnORasBptSATBlnN9ArVZCDNqCe8HOjsMeFrZCYR16FaXfDsOYsZAZAXij2xEanU6wZB38NESB2qYfD7qJeFD3RGkZANayCozAwA6KBU5smOZA1tveOhU00AuDVuzQbSIb6ZCZC26wkJPYoZA7O3T7eGxzBTaa0bJ7LIVamhZBP3TyDhd7KkgrwqYP8wZDZD"
+page_access_tkn = os.getenv('PAGE_ACCESS_TOKEN')
+verify_token = os.getenv('VERIFY_TOKEN')
 
 @app.route('/')
 def index():
@@ -13,13 +16,14 @@ def index():
 
 @app.route('/webhook', methods=['POST','GET'])
 def webhook():
+    # verification
     if request.method == "GET":
-        if request.args.get("hub.verify_token") == "hologo":
+        if request.args.get("hub.verify_token") == verify_token:
             return request.args.get("hub.challenge")
         else:
             return request
+    # receives msgs
     elif request.method == 'POST':
-        # Handle incoming messages
         data = request.json
         #print("Received payload:", data)  
         if 'entry' in data:
@@ -35,14 +39,14 @@ def webhook():
 
         return 'OK', 200
     
-@app.route('/fb/messages')
+@app.route('/fb/msgs')
 def fb_messages():
     external_url = 'https://graph.facebook.com/v19.0/me/conversations?fields=messages%7Bid%2Cfrom%2Cto%2Cmessage%7D&platform=messenger&'+f'access_token={page_access_tkn}'  
     response = requests.get(external_url)
     data = response.json()
     return jsonify(data)
 
-@app.route('/instagram/messages')
+@app.route('/ig/msgs')
 def instagram_messages():
     external_url = 'https://graph.facebook.com/v19.0/me/conversations?fields=messages%7Bid%2Cfrom%2Cto%2Cmessage%7D&platform=instagram&'+f'access_token={page_access_tkn}'  
     response = requests.get(external_url)
